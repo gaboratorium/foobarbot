e69f38204967f5e362779e6106acf6d9d3c87b90
Gabor Pinter <gaboratorium@gmail.com>
2016-10-04 17:05:29 +0200

    Example NodeJS app says Hello

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello");
+// Imports
+var express = require('express');
+
+// Express configuration
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(5000, function (data) {
+  console.log(`Example app listening!`);
+});
