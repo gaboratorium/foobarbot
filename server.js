1c96453a3e0d318f511305c73e363842dde13e02
Gabor Pinter <gaboratorium@gmail.com>
2016-10-05 20:20:57 +0200

    Nodemon and express installed, static file served

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 // Imports
 var express = require('express');
+var fs = require('fs');
 
 // Express configuration
 var app = express();
 
+// Load index.html
+var indexHtml = fs.readFileSync(__dirname + "/public/index.html", "utf8");
+
 app.get('/', function (req, res) {
-  res.send('Hello World, this is Project Lines!');
+  res.send(indexHtml);
 });
 
 app.listen(process.env.PORT || 5000, function (data) {
