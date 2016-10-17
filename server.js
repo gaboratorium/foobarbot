dc4d535e344963d0926ac73fba37f57c3b9c0e9f
Gabor Pinter <gaboratorium@gmail.com>
2016-10-17 19:30:38 +0200

    Api routes created

--- server.js
+++ server.js
@@ -66,18 +66,74 @@ var morgan      = require('morgan');
 // use body parser so we can get info from POST and/or URL parameters
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// Start morgan log
+// Start morgan logweb
 app.use(morgan('dev'));
 
+//  Mongoose init
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
-app.get('/api/test', function(req, res){
+// Connect to db with mongoose
+mongoose.connect("mongodb://heroku_6lt22ghm:te2b1dta8i2glj7ss4lk71vjnm@ds037814.mlab.com:37814/heroku_6lt22ghm");
+
+
+
+// Set a secret
+app.set('superSecret', 'supersuperserverserverseecret');
+
+// Set up API routes
+var apiRoutes = express.Router();
+
+// apply the routes to our application with the prefix /api
+app.use('/api', apiRoutes);
+
+apiRoutes.get('/setup', function(req, res) {
+
+	// Create mongoose model
+	var User = mongoose.model('User', new Schema({ 
+	    name: String, 
+	    password: String, 
+	    admin: Boolean 
+	}));
+
+  // create a sample user
+  var nick = new User({ 
+    name: 'Nick Cerminara', 
+    password: 'password',
+    admin: true 
+  });
+
+  // save the sample user
+  nick.save(function(err) {
+    if (err) throw err;
+
+    console.log('User saved successfully');
+    res.json({ success: true });
+  });
+});
+
+
+// route to show a random message (GET http://localhost:3000/api/)
+apiRoutes.get('/', function(req, res) {
+  res.json({ message: 'Welcome to the coolest API on earth!' });
+});
+
+// route to return all users (GET http://localhost:8080/api/users)
+apiRoutes.get('/users', function(req, res) {
+  User.find({}, function(err, users) {
+    res.json(users);
+  });
+});
+
+
+
+apiRoutes.get('/test', function(req, res){
 	var testObj = { 'name': 'Gabor', 'age': 24 };
 	res.setHeader("Content-Type", "application/json");
 	testObjAsString = JSON.stringify(testObj);
 	res.send(testObjAsString);
 })
 
-app.set('superSecret', 'supersuperserverserverseecret');
 
 ////////////////////////////////////////////////////
 ////////////////////////////////////////////////////
@@ -88,4 +144,4 @@ app.listen(process.env.PORT || defaultPort, function (data) {
 });
 
 //run the database.js file
-var database = require('./app/server/src/database.js');
+// var database = require('./app/server/src/database.js');
