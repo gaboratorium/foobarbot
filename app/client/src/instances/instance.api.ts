dabc7ce0881b6ff7f57f83ef0cd7282ec483c087
Gabor Pinter <gaboratorium@gmail.com>
2016-11-04 08:49:33 +0100

    Api tsified, vue-resource type definitions installed

--- app/client/src/instances/instance.api.ts
+++ app/client/src/instances/instance.api.ts
@@ -1,5 +1,3 @@
-/// <reference path="./../../../../node_modules/vue/types/index.d.ts" />
-
 export const ApiInstance = new Vue({
 	name: "Api",
 	methods: {
@@ -8,10 +6,10 @@ export const ApiInstance = new Vue({
 		},
 
 		// Verify token
-		verifyToken: (myToken) => {
+		verifyToken: (myToken: any) => {
 			var body = { token: myToken };
 			var myPromise = new Promise((resolve, reject) => {
-				Vue.http.post('api/token/verify', body).then((response) => {
+				Vue.http.post('api/token/verify', body).then((response: any) => {
 					resolve(response.body);
 				}, (fail) => {
 					reject(fail);
@@ -21,26 +19,26 @@ export const ApiInstance = new Vue({
 		},
 
 		// Create token
-		createToken: (myUserEmail, myUserPassword) => {
+		createToken: (myUserEmail: any, myUserPassword: any) => {
 			var body = {userEmail: myUserEmail, userPassword: myUserPassword};
 			var myPromise = new Promise((resolve, reject) => {
-				Vue.http.post('api/token/create', body).then((response) => {
+				Vue.http.post('api/token/create', body).then((response: any) => {
 					resolve(response.body);
-				}, (fail) => {
+				}, (fail: any) => {
 					reject(fail);
 				});
 			});
 			return myPromise;
 		},
 
-		loadUsers: (myToken) => {
+		loadUsers: (myToken: any) => {
 			var options = { 
 				headers: {
 					'x-access-token': myToken
 				} 
 			};			
 			var myPromise = new Promise((resolve, reject) => {
-				Vue.http.get('/api/users', options).then((response) => {
+				Vue.http.get('/api/users', options).then((response: any) => {
 					resolve(response.body);
 
 				}, (fail) => {
@@ -50,7 +48,7 @@ export const ApiInstance = new Vue({
 			return myPromise;
 		},
 		
-		getNotifications: (myUserToken, myUserEmail) => {
+		getNotifications: (myUserToken: any, myUserEmail: any) => {
 
 			var options = { 
 				params: {
@@ -77,7 +75,7 @@ export const ApiInstance = new Vue({
 			return myPromise;
 		},
 
-		postNotification: (myToken, myUserEmail, myMessage) => {
+		postNotification: (myToken: any, myUserEmail: any, myMessage: any) => {
 			
 			var body = {
 				userEmail: myUserEmail,
@@ -89,7 +87,7 @@ export const ApiInstance = new Vue({
 			
 
 			var myPromise = new Promise((resolve, reject) => {
-				Vue.http.post('/api/notifications', body).then((response) => {
+				Vue.http.post('/api/notifications', body).then((response: any) => {
 					resolve(response.body);
 				}, (fail) => {
 					reject(fail);
@@ -98,7 +96,7 @@ export const ApiInstance = new Vue({
 			return myPromise;
 		},
 
-		deleteNotification: (myUserEmail, myToken) => {
+		deleteNotification: (myUserEmail: any, myToken: any) => {
 			var body = {
 				token: myToken,
 				userEmail: myUserEmail
@@ -109,7 +107,7 @@ export const ApiInstance = new Vue({
 				body: body
 			};
 			var myPromise = new Promise((resolve, reject) => {
-				Vue.http.delete('/api/notifications', options).then((response) => {
+				Vue.http.delete('/api/notifications', options).then((response: any) => {
 					resolve(response.body);
 				}, (fail) => {
 					reject(fail);
@@ -118,7 +116,7 @@ export const ApiInstance = new Vue({
 			return myPromise;
 		},
 
-		signupUser: (myUserName, myUserEmail, myUserPassword) => {
+		signupUser: (myUserName: any, myUserEmail: any, myUserPassword: any) => {
 			var body = {
 				userName: myUserName,
 				userEmail: myUserEmail,
@@ -126,7 +124,7 @@ export const ApiInstance = new Vue({
 			};
 
 			var myPromise = new Promise((resolve, reject) => {
-				Vue.http.post('/api/users/', body).then((response) => {
+				Vue.http.post('/api/users/', body).then((response: any) => {
 					resolve(response.body);
 				}, (fail) => {
 					reject(fail);
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,6 @@
 /// <reference path="globals/es6-promise/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="globals/require/index.d.ts" />
+/// <reference path="globals/vue-resource/index.d.ts" />
 /// <reference path="globals/vue-router/index.d.ts" />
 /// <reference path="globals/vue/index.d.ts" />
