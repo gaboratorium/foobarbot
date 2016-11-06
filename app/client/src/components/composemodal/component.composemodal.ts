5a3b0ced6e5538d60f5f3b2991120dfa2670d636
Gabor Pinter <gaboratorium@gmail.com>
2016-11-06 07:46:03 +0100

    modal progmmatic form submit

--- app/client/src/components/composemodal/component.composemodal.ts
+++ app/client/src/components/composemodal/component.composemodal.ts
@@ -3,5 +3,17 @@ var html = fs.readFileSync(__dirname + '/component.composemodal.html', 'utf8');
 
 export const ComposeModalComponent = {
     name: "modal",
-    template: html
+    template: html,
+    methods: {
+        postSnippet: function(){
+            console.log("YYou are trying t o post a snippet");
+            this.$emit("close");
+            
+        },
+
+        closeModal: function(){
+            console.log("You are trying to close the modal...");
+            this.$emit("close");
+        }
+    }
 }
