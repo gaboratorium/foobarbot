1a503f8cf1dcf3c40e756c91dd77708d1c777beb
Gabor Pinter <gaboratorium@gmail.com>
2016-11-06 20:42:49 +0100

    getting data from modal

--- app/client/src/components/composemodal/component.composemodal.ts
+++ app/client/src/components/composemodal/component.composemodal.ts
@@ -4,9 +4,22 @@ var html = fs.readFileSync(__dirname + '/component.composemodal.html', 'utf8');
 export const ComposeModalComponent = {
     name: "modal",
     template: html,
+    data: function(){
+        return {
+            composeform__snippet: "",
+			composeform__tag1: "",
+			composeform__tag2: "",
+			composeform__tag3: "",
+			errorMsg: ""
+        }
+    },
     methods: {
         postSnippet: function(){
             console.log("YYou are trying t o post a snippet");
+            console.log(this.composeform__tag1);
+            console.log(this.composeform__tag2);
+            console.log(this.composeform__tag3);
+            console.log(this.composeform__snippet);
             this.$emit("close");
             
         },
