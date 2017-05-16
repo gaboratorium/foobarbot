402785f3b1b078b2d2e709976016c5ae27101346
gaboratorium <gaboratorium>
2017-05-16 06:37:55 -0700

    Bit of a cleanup

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 /*jshint esversion: 6 */
 
-// Import modules
 var express = require('express');
 var fs = require('fs');
 var passwordHash = require('password-hash');
 var _ = require('lodash');
-
-// Init express app
+var bodyParser = require('body-parser');
+var jwt = require('jsonwebtoken');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
 var app = express();
 
-// Define globalPaths
 var globalPath = {
 	"client": {
 		"root": "/app/client/",
@@ -21,51 +21,23 @@ var globalPath = {
 	}
 };
 
+const FOOBARBOT_ID = "1479481497854175";
 var defaultPort = 3000;
+var indexHtml = fs.readFileSync(__dirname + globalPath.client.dist + "index.html", "utf8");
 
+app.get('/', function (req, res) { res.send(indexHtml); });
 app.use('/js', express.static(__dirname + globalPath.client.dist + "js/"));
 app.use('/css', express.static(__dirname + globalPath.client.dist + "css/"));
 app.use('/assets', express.static(__dirname + globalPath.client.dist + "assets/"));
-
-// Prepare to deliver for client, save to RAM
-var indexHtml = fs.readFileSync(__dirname + globalPath.client.dist + "index.html", "utf8");
-
-const FOOBARBOT_ID = "1479481497854175";
-
-
-
-
-// Serving index
-app.get('/', function (req, res) {
-  res.send(indexHtml);
-});
-
-////////////////////////////////////////////////////
-////////////////////////////////////////////////////
-// API and authentication
-//body parser to parameters from POST and URL
-var bodyParser  = require('body-parser');
-// json web token authentication
-var jwt    = require('jsonwebtoken');
-// log requests to the console
-var morgan      = require('morgan');
-
-// use body parser so we can get info from POST and/or URL parameters
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// Start morgan logweb
 app.use(morgan('dev'));
+app.set('superSecret', 'supersuperserverserverseecret');
 
-//  Mongoose init
-var mongoose = require('mongoose');
+
 var Schema = mongoose.Schema;
-
-// Connect to db with mongoose
 mongoose.connect("mongodb://heroku_6lt22ghm:te2b1dta8i2glj7ss4lk71vjnm@ds037814.mlab.com:37814/heroku_6lt22ghm");
 
-/////////////////////////////////////
-// Models
-// User model 
 var User = mongoose.model('User', new Schema({
 	userId: String,
     userName: String, 
@@ -75,14 +47,12 @@ var User = mongoose.model('User', new Schema({
 	registrationDate: Number
 }));
 
-// Star model
 var Star = mongoose.model('Star', new Schema({
 	userId: String, 
     snippetId: String, 
     date: Number
 }));
 
-// Snippet model
 var Snippet = mongoose.model('snippet', new Schema({
 	snippetId: String,
 	snippetCode: String,
@@ -94,28 +64,17 @@ var Snippet = mongoose.model('snippet', new Schema({
 	vendor: Boolean
 }));
 
-// Notification model
 var Notification = mongoose.model('Notification', new Schema({
 	userEmail: String, 
     message: String, 
     date: Number 
 }));
 
-
-// Set a secret
-app.set('superSecret', 'supersuperserverserverseecret');
-
-// Set up API routes
 var apiRoutes = express.Router();
 
-// apply the routes to our application with the prefix /api
 app.use('/api', apiRoutes);
 
-apiRoutes.post('/token/create', function(req, res){
-
-
-	
-
+apiRoutes.post('/token/create', function(req, res) {
 	User.findOne({userEmail: req.body.userEmail}, function(err, user){
 		if (err) {
 			res.status(500).send({success: false, message: "Something went wrong"});
@@ -124,19 +83,16 @@ apiRoutes.post('/token/create', function(req, res){
 
 		if (user && passwordHash.verify(req.body.userPassword, user.userPassword)) {
 
-			// Create user object to encode in token
 			var userToEncode = {
 				"userId": user.userId,
 				"userEmail": user.userEmail,
 				"userName": user.userName
 			};
 
-			// Create token with user object encoded in it
 			var token = jwt.sign(userToEncode, app.get('superSecret'), {
 				expiresIn : 1440 // 24 hours
 			});
 
-			// Create userClient with user object and token
 			var myUserClient = {
 				userToken: token,
 				userId: user.userId,
@@ -144,27 +100,21 @@ apiRoutes.post('/token/create', function(req, res){
 				userName: user.userName
 			};
 
-			// Send back token
 			res.json({success: true, message: "Good request", userClient: myUserClient});
-		}
-		else {
+		} else {
 			res.status(409).send({success: false, message: "User was not found with this e-mail and password"});
 		}
 	});
 });
 
 apiRoutes.post('/token/verify', function(req, res){
-	
-	// check header or url parameters or post parameters for token
 	var token = req.body.token || req.query.token || req.headers['x-access-token'];	
-
 	if (token){
 		jwt.verify(token, app.get('superSecret'), function(err, decoded){
 			if (err) {
 				return res.json({success: false, message: 'Failed to authenticate token...'});
 			} else {
 				var decodedToken = jwt.verify(token, app.get('superSecret'));
-				
 				req.decode = req;
 
 				var myUserClient = {
@@ -180,9 +130,7 @@ apiRoutes.post('/token/verify', function(req, res){
 	}
 });
 
-// Registration, signup
 apiRoutes.post('/users', function(req, res) {
-	
 	var newObj = {
 		userId: String(new Date().getTime())+String(Math.floor(Math.random()*1000)),
 		userName : req.body.userName,
@@ -207,7 +155,6 @@ apiRoutes.post('/users', function(req, res) {
 	});
 });
 
-
 apiRoutes.get('/user', function(req, res) {
 	var myUserId = req.query.userId;
 	
@@ -221,13 +168,9 @@ apiRoutes.get('/user', function(req, res) {
 	});
 });
 
-// Get snippets from user Id
 apiRoutes.get('/snippets', function(req, res) {
-	
-	// IF userId or snippetId is provided
 	var snippetsMaxNumber = req.query.snippetsMaxNumber ? req.query.snippetsMaxNumber : 100;
 	var searchText = req.query.searchText ? req.query.searchText : null;
-
 
 	var options = {};
 	if (req.query.userId) {
@@ -237,7 +180,6 @@ apiRoutes.get('/snippets', function(req, res) {
 	}
 
 	var mySnippet = new Snippet(req.body.snippet);
-
 	
 	Snippet.find(options, function(err, snippets){
 		if (snippets) {
@@ -260,15 +202,11 @@ apiRoutes.get('/snippets', function(req, res) {
 });
 
 apiRoutes.get('/starredsnippets', function(req, res) {
-
-	
-
 	var snippetsMaxNumber = req.query.snippetsMaxNumber ? req.query.snippetsMaxNumber : 10;
 	var myUserId = req.query.userId;
 	var snippets = [];
-
-
 	var myPromises = [];
+
 	Star.find({userId: myUserId}, (err, stars) => {
 		
 		if (err) throw err;
@@ -285,8 +223,6 @@ apiRoutes.get('/starredsnippets', function(req, res) {
 
 		}
 
-
-
 		Promise.all(myPromises).then(resolvings => {
 			return res.json({succes: true, message: "You know some shit", snippets: resolvings});
 		}).catch(reason => {
@@ -295,11 +231,8 @@ apiRoutes.get('/starredsnippets', function(req, res) {
 	});
 });
 
-// route middleware to verify a token
-apiRoutes.use(function(req, res, next){
 
-	
-	// check header or url parameters or post parameters for token
+apiRoutes.use(function(req, res, next) {
 	var token = req.body.token || req.query.token || req.headers['x-access-token'] || req.body.userToken;
 
 	if (token){
@@ -319,25 +252,6 @@ apiRoutes.use(function(req, res, next){
 		return res.status(403).send({success: false, message: "No token provided."});
 	}
 });
-
-// apiRoutes.get('/setup', function(req, res) {
-
-	
-
-//   // create a sample user
-//   var nick = new User({ 
-//     name: 'Nick Cerminara', 
-//     password: 'password',
-//     admin: true 
-//   });
-
-//   // save the sample user
-//   nick.save(function(err) {
-//     if (err) throw err;
-
-//     res.json({ success: true });
-//   });
-// });
 
 apiRoutes.post('/foobarbotsnippet', function(req, res) {
 	var mySnippet = req.body.snippet;
@@ -374,24 +288,13 @@ apiRoutes.post('/foobarbotsnippet', function(req, res) {
 	})
 })
 
-
-// route to show a random message (GET http://localhost:3000/api/)
-apiRoutes.get('/', function(req, res) {
-  res.json({ message: 'Welcome to the coolest API on earth!' });
-});
-
-// route to return all users (GET http://localhost:8080/api/users)
 apiRoutes.get('/users', function(req, res) {
   User.find({}, function(err, users) {
-
-	
     res.json(users);
   });
 });
 
-// route to return all notifications from admin (GET http://localhost:8080/api/users/admin/notifications)
 apiRoutes.get('/notifications', function(req, res) {
-	
 	myUserEmail = req.query.userEmail;
 	Notification.find({userEmail: myUserEmail}, function(err, notifications) {
 		if (err) {
@@ -439,12 +342,7 @@ apiRoutes.post('/stars', function(req, res) {
 
 // route to create a new notifications
 apiRoutes.post('/notifications', function(req, res) {
-
-
 	var userEmail = req.body.userEmail;
-	
-
-	
 	var notificationMessage = req.body.notificationMessage;
 
 	var myNotification = new Notification({
@@ -511,10 +409,6 @@ apiRoutes.delete('/user', function(req, res) {
 		})
 	});
 });
-
-
-////////////////////////////////////////////////////
-////////////////////////////////////////////////////
 
 // Start listening on port 5000
 app.listen(process.env.PORT || defaultPort, function (data) {
